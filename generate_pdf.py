4b4e3d39592aab83b34261f4ddaf51cde5c512ec
Albert Carlson <ac.albertcarlson@gmail.com>
2024-04-29 22:46:20 +0200

    testing etc

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,12 +1,13 @@
-from problems import ISL2022_NO_G, ISL2021_NO_G, ISL2020_NO_G, ISL2019_NO_G
+from problems import ISL2022_NO_G, ISL2021_NO_G, ISL2020_NO_G, ISL2019_NO_G, ISL2017_NO_G, ISL2018_NO_G
 
+ISL = ISL2018_NO_G + ISL2017_NO_G
 
-ISL2019_NO_G.to_pdf(
+ISL.to_pdf(
     topics=("A", "C", "N"),
     include_unlabelled=False,
     sources=False,
     randomize_order=False,
     remove_crap=True,
-    output_filename="2019",
+    output_filename="2017_2018",
     #num_problems=8,
 )
